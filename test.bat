08ad2bea937e9162c517b155a434826c92c71abc
Coby Vardy <coby.vardy@gmail.com>
2022-07-31 16:16:34 -0700

    check check

--- test.bat
+++ test.bat
@@ -3,7 +3,7 @@ echo hello
 cd C:\Users\Coby.Vardy\Documents\coding\coding-notes
 set /P title=Enter commit description:
 git add --all
-git commit -m %title%
+git commit -m "%title%"
 git push
 echo coding-notes %title% commit has completed successfully!
 timeout /t 3
