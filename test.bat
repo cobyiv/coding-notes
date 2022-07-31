4cb3f18668d0f43ebdeaa20816420d38e1e9e692
Coby Vardy <coby.vardy@gmail.com>
2022-07-31 16:19:41 -0700

    .gitignore update

--- test.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo hello
-cd C:\Users\Coby.Vardy\Documents\coding\coding-notes
-set /P title=Enter commit description:
-git add --all
-git commit -m "%title%"
-git push
-echo coding-notes %title% commit has completed successfully!
-timeout /t 3
